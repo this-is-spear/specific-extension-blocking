5db8bca2928e12b75ad508dbb5a6ec402c238143
this-is-spear <rjsckdd12@gmail.com>
2022-11-22 23:02:36 +0900

    Refact : Cheked Lint

--- src/main/java/hello/tis/specificextensionblocking/SpecificExtensionBlockingApplication.java
+++ src/main/java/hello/tis/specificextensionblocking/SpecificExtensionBlockingApplication.java
@@ -3,11 +3,14 @@ package hello.tis.specificextensionblocking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 특정 확장자를 제한하기 위해 만들어진 서비스입니다.
+ */
 @SpringBootApplication
 public class SpecificExtensionBlockingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpecificExtensionBlockingApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpecificExtensionBlockingApplication.class, args);
+  }
 
 }
