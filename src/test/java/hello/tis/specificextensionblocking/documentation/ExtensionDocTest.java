c2f6dbab12b2c79ff10440f22293ee48f9aeeaad
this-is-spear <rjsckdd12@gmail.com>
2022-11-23 13:45:53 +0900

    Init : API 문서 초기세팅

---
+++ src/test/java/hello/tis/specificextensionblocking/documentation/ExtensionDocTest.java
@@ -0,0 +1,26 @@
+package hello.tis.specificextensionblocking.documentation;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 확장자 리스트 조회(GET : /extensions)*
+ * 확장자 추가(POST : /extensions)*
+ * 확장자 삭제(DELETE : /extensions)*
+ */
+class ExtensionDocTest {
+
+  @Test
+  void findExtension() {
+
+  }
+
+  @Test
+  void addExtension() {
+
+  }
+
+  @Test
+  void deleteExtension() {
+
+  }
+}
