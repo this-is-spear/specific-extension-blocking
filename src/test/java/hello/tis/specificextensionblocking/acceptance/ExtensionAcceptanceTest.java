37365e5c57d2fdf96bdaa21e55a2a948d8edcf04
this-is-spear <rjsckdd12@gmail.com>
2022-11-23 17:09:23 +0900

    Test : Added AeeptanceTest

--- src/test/java/hello/tis/specificextensionblocking/acceptance/ExtensionAcceptanceTest.java
+++ src/test/java/hello/tis/specificextensionblocking/acceptance/ExtensionAcceptanceTest.java
@@ -1,18 +1,27 @@
 package hello.tis.specificextensionblocking.acceptance;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 class ExtensionAcceptanceTest {
+
+  @Autowired
+  ObjectMapper objectMapper;
 
   @Autowired
   MockMvc mockMvc;
@@ -33,7 +42,29 @@ class ExtensionAcceptanceTest {
    */
   @Test
   @Disabled
-  void test1() {
+  void test1() throws Exception {
+    final String 고정_확장자 = "sh";
+    // when
+    ResultActions 확장자_추가 = 확장자_추가(고정_확장자);
+    확장자_추가.andExpect(status().isOk());
+
+    // then
+    String 설정_후_조회_결과 = 조회_요청(고정_확장자)
+        .andReturn().getResponse().getContentAsString();
+
+    // TODO : 결과 비교 구현
+    System.out.println(설정_후_조회_결과);
+
+    // when
+    ResultActions 확장자_해제 = 확장자_해제(고정_확장자);
+    확장자_해제.andExpect(status().isOk());
+
+    // then
+    String 해제_후_조회_결과 = 조회_요청(고정_확장자)
+        .andReturn().getResponse().getContentAsString();
+
+    // TODO : 결과 비교 구현
+    System.out.println(해제_후_조회_결과);
   }
 
   /**
@@ -42,7 +73,14 @@ class ExtensionAcceptanceTest {
    */
   @Test
   @Disabled
-  void test2() {
+  void test2() throws Exception {
+    String 설정되지_않은_고정_확장자 = "exe";
+
+    // when
+    ResultActions 확장자_해제 = 확장자_해제(설정되지_않은_고정_확장자);
+
+    // then
+    확장자_해제.andExpect(status().is4xxClientError());
   }
 
   /**
@@ -51,7 +89,17 @@ class ExtensionAcceptanceTest {
    */
   @Test
   @Disabled
-  void test3() {
+  void test3() throws Exception {
+    // given
+    String 설정된_고정_확장자 = "sh";
+    ResultActions 확장자_추가_요청 = 확장자_추가(설정된_고정_확장자);
+    확장자_추가_요청.andExpect(status().isOk());
+
+    // when
+    ResultActions 잘못된_확장자_추가_요청 = 확장자_추가(설정된_고정_확장자);
+
+    // then
+    잘못된_확장자_추가_요청.andExpect(status().is4xxClientError());
   }
 
   /**
@@ -62,16 +110,46 @@ class ExtensionAcceptanceTest {
    */
   @Test
   @Disabled
-  void test4() {
+  void test4() throws Exception {
+    String 커스텀_확장자 = "yml";
+    // when
+    ResultActions 확장자_추가 = 확장자_추가(커스텀_확장자);
+    확장자_추가.andExpect(status().isOk());
+
+    // then
+    String 설정_후_조회_결과 = 조회_요청(커스텀_확장자)
+        .andReturn().getResponse().getContentAsString();
+
+    // TODO : 결과 비교 구현
+    System.out.println(설정_후_조회_결과);
+
+    // when
+    ResultActions 확장자_해제 = 확장자_해제(커스텀_확장자);
+    확장자_해제.andExpect(status().isOk());
+
+    // then
+    String 해제_후_조회_결과 = 조회_요청(커스텀_확장자)
+        .andReturn().getResponse().getContentAsString();
+
+    // TODO : 결과 비교 구현
+    System.out.println(해제_후_조회_결과);
+
   }
 
   /**
-   * @When : 설정되지 않은 커스텀 확장자를 하제하려하면
+   * @When : 설정되지 않은 커스텀 확장자를 해제하려하면
    * @Then : 예외가 발생한다.
    */
   @Test
   @Disabled
-  void test5() {
+  void test5() throws Exception {
+    String 설정되지_않은_커스텀_확장자 = "c";
+
+    // when
+    ResultActions 확장자_해제 = 확장자_해제(설정되지_않은_커스텀_확장자);
+
+    // then
+    확장자_해제.andExpect(status().is4xxClientError());
   }
 
   /**
@@ -80,29 +158,42 @@ class ExtensionAcceptanceTest {
    */
   @Test
   @Disabled
-  void test6() {
+  void test6() throws Exception {
+    // given
+    String 설정된_커스텀_확장자 = "yml";
 
+    ResultActions 확장자_추가_요청 = 확장자_추가(설정된_커스텀_확장자);
+    확장자_추가_요청.andExpect(status().isOk());
+
+    // when
+    ResultActions 잘못된_확장자_추가_요청 = 확장자_추가(설정된_커스텀_확장자);
+
+    // then
+    잘못된_확장자_추가_요청.andExpect(status().is4xxClientError());
   }
 
-  /**
-   * @When : 커스텀 확장자에 고정 확장자를 추가할 때,
-   * @When : 고정 확장자가 설정되지 않았다면
-   * @Then : 고정 확장자를 설정한다.
-   */
-  @Test
-  @Disabled
-  void test7() {
-
+  private ResultActions 조회_요청(String extension) throws Exception {
+    return mockMvc.perform(
+        MockMvcRequestBuilders.get("/extensions")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("\"name\":\"" + extension + "\"")
+    ).andExpect(status().isOk());
   }
 
-  /**
-   * @When : 커스텀 확장자에 고정 확장자를 추가할 때,
-   * @When : 고정 확장자가 설정되어 있다면
-   * @Then : 예외가 발생한다.
-   */
-  @Test
-  @Disabled
-  void test8() {
-
+  private ResultActions 확장자_추가(String extension) throws Exception {
+    return mockMvc.perform(
+        MockMvcRequestBuilders.post("/extensions")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("\"name\":\"" + extension + "\"")
+    );
   }
+
+  private ResultActions 확장자_해제(String extension) throws Exception {
+    return mockMvc.perform(
+        MockMvcRequestBuilders.delete("/extensions")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("\"name\":\"" + extension + "\"")
+    );
+  }
+
 }
