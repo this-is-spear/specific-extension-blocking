cad504352c9e0e36c519d5d663b44ae5e4bbaf6a
this-is-spear <rjsckdd12@gmail.com>
2022-11-23 17:14:33 +0900

    Refact : Refactoring AcceptanceTest

--- src/test/java/hello/tis/specificextensionblocking/acceptance/ExtensionAcceptanceTest.java
+++ src/test/java/hello/tis/specificextensionblocking/acceptance/ExtensionAcceptanceTest.java
@@ -5,7 +5,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +20,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 class ExtensionAcceptanceTest {
+
+  private static final String 고정_확장자 = "sh";
+  private static final String 커스텀_확장자 = "yml";
+  private static final String 설정된_고정_확장자 = "sh";
+  private static final String 설정되지_않은_고정_확장자 = "exe";
+  private static final String 설정되지_않은_커스텀_확장자 = "c";
+  private static final String 설정된_커스텀_확장자 = "yml";
 
   @Autowired
   ObjectMapper objectMapper;
@@ -35,32 +43,32 @@ class ExtensionAcceptanceTest {
   }
 
   /**
-   * @When : 고정 확장자를 설정하면
+   * @When : 확장자를 설정하면
    * @Then : 설정된 확장자에 확장자가 추가되고,
-   * @When : 설정된 고정 확장자를 해제하면
+   * @When : 설정된 확장자를 해제하면
    * @Then : 설정된 확장자에서 삭제된다.
    */
-  @Test
   @Disabled
-  void test1() throws Exception {
-    final String 고정_확장자 = "sh";
+  @ParameterizedTest
+  @ValueSource(strings = {고정_확장자, 커스텀_확장자})
+  void test1(String 확장자) throws Exception {
     // when
-    ResultActions 확장자_추가 = 확장자_추가(고정_확장자);
+    ResultActions 확장자_추가 = 확장자_추가(확장자);
     확장자_추가.andExpect(status().isOk());
 
     // then
-    String 설정_후_조회_결과 = 조회_요청(고정_확장자)
+    String 설정_후_조회_결과 = 조회_요청(확장자)
         .andReturn().getResponse().getContentAsString();
 
     // TODO : 결과 비교 구현
     System.out.println(설정_후_조회_결과);
 
     // when
-    ResultActions 확장자_해제 = 확장자_해제(고정_확장자);
+    ResultActions 확장자_해제 = 확장자_해제(확장자);
     확장자_해제.andExpect(status().isOk());
 
     // then
-    String 해제_후_조회_결과 = 조회_요청(고정_확장자)
+    String 해제_후_조회_결과 = 조회_요청(확장자)
         .andReturn().getResponse().getContentAsString();
 
     // TODO : 결과 비교 구현
@@ -68,109 +76,40 @@ class ExtensionAcceptanceTest {
   }
 
   /**
-   * @When : 설정되지 않은 고정 확장자를 해제하려하면
+   * @When : 설정되지 않은 확장자를 해제하려하면
    * @Then : 예외가 발생한다.
    */
-  @Test
   @Disabled
-  void test2() throws Exception {
-    String 설정되지_않은_고정_확장자 = "exe";
+  @ParameterizedTest
+  @ValueSource(strings = {설정되지_않은_고정_확장자, 설정되지_않은_커스텀_확장자})
+  void test2(String 설정되지_않은_확장자) throws Exception {
 
     // when
-    ResultActions 확장자_해제 = 확장자_해제(설정되지_않은_고정_확장자);
+    ResultActions 확장자_해제 = 확장자_해제(설정되지_않은_확장자);
 
     // then
     확장자_해제.andExpect(status().is4xxClientError());
   }
 
   /**
-   * @When : 설정된 고정 확장자를 설정하려하면
+   * @When : 설정된 확장자를 설정하려하면
    * @Then : 예외가 발생한다.
    */
-  @Test
   @Disabled
-  void test3() throws Exception {
+  @ParameterizedTest
+  @ValueSource(strings = {설정된_고정_확장자, 설정된_커스텀_확장자})
+  void test3(String 설정된_확장자) throws Exception {
     // given
-    String 설정된_고정_확장자 = "sh";
-    ResultActions 확장자_추가_요청 = 확장자_추가(설정된_고정_확장자);
+    ResultActions 확장자_추가_요청 = 확장자_추가(설정된_확장자);
     확장자_추가_요청.andExpect(status().isOk());
 
     // when
-    ResultActions 잘못된_확장자_추가_요청 = 확장자_추가(설정된_고정_확장자);
+    ResultActions 잘못된_확장자_추가_요청 = 확장자_추가(설정된_확장자);
 
     // then
     잘못된_확장자_추가_요청.andExpect(status().is4xxClientError());
   }
 
-  /**
-   * @When : 커스텀 확장자를 추가하면
-   * @Then : 설정된 확장자에 확장자가 추가되고,
-   * @When : 커스텀 확장자를 삭제하면
-   * @Then : 설정된 확장자에서 삭제된다.
-   */
-  @Test
-  @Disabled
-  void test4() throws Exception {
-    String 커스텀_확장자 = "yml";
-    // when
-    ResultActions 확장자_추가 = 확장자_추가(커스텀_확장자);
-    확장자_추가.andExpect(status().isOk());
-
-    // then
-    String 설정_후_조회_결과 = 조회_요청(커스텀_확장자)
-        .andReturn().getResponse().getContentAsString();
-
-    // TODO : 결과 비교 구현
-    System.out.println(설정_후_조회_결과);
-
-    // when
-    ResultActions 확장자_해제 = 확장자_해제(커스텀_확장자);
-    확장자_해제.andExpect(status().isOk());
-
-    // then
-    String 해제_후_조회_결과 = 조회_요청(커스텀_확장자)
-        .andReturn().getResponse().getContentAsString();
-
-    // TODO : 결과 비교 구현
-    System.out.println(해제_후_조회_결과);
-
-  }
-
-  /**
-   * @When : 설정되지 않은 커스텀 확장자를 해제하려하면
-   * @Then : 예외가 발생한다.
-   */
-  @Test
-  @Disabled
-  void test5() throws Exception {
-    String 설정되지_않은_커스텀_확장자 = "c";
-
-    // when
-    ResultActions 확장자_해제 = 확장자_해제(설정되지_않은_커스텀_확장자);
-
-    // then
-    확장자_해제.andExpect(status().is4xxClientError());
-  }
-
-  /**
-   * @When : 설정된 커스텀 확장자를 설정하려하면
-   * @Then : 예외가 발생한다.
-   */
-  @Test
-  @Disabled
-  void test6() throws Exception {
-    // given
-    String 설정된_커스텀_확장자 = "yml";
-
-    ResultActions 확장자_추가_요청 = 확장자_추가(설정된_커스텀_확장자);
-    확장자_추가_요청.andExpect(status().isOk());
-
-    // when
-    ResultActions 잘못된_확장자_추가_요청 = 확장자_추가(설정된_커스텀_확장자);
-
-    // then
-    잘못된_확장자_추가_요청.andExpect(status().is4xxClientError());
-  }
 
   private ResultActions 조회_요청(String extension) throws Exception {
     return mockMvc.perform(
