48ba91051a033c21dff43d2d29bc9af528d5ac93
this-is-spear <rjsckdd12@gmail.com>
2022-11-23 13:58:09 +0900

    Feat : ExtensionController 구현

---
+++ src/main/java/hello/tis/specificextensionblocking/api/ui/ExtensionRequest.java
@@ -0,0 +1,8 @@
+package hello.tis.specificextensionblocking.api.ui;
+
+/**
+ * 확장자를 추가하기 위한 DTO 입니다.
+ */
+public class ExtensionRequest {
+
+}
---
+++ src/main/java/hello/tis/specificextensionblocking/api/ui/ExtensionResponses.java
@@ -0,0 +1,8 @@
+package hello.tis.specificextensionblocking.api.ui;
+
+/**
+ * 확장자 리스트를 반환하기 위한 DTO 입니다.
+ */
+public class ExtensionResponses {
+
+}
