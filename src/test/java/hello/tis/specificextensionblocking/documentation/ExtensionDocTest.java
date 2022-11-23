d5e78e49f751ee40cc4075ac56f718a4bc01aeb1
this-is-spear <rjsckdd12@gmail.com>
2022-11-23 13:58:42 +0900

    ExtensionDocTest 주석 수정

--- src/test/java/hello/tis/specificextensionblocking/documentation/ExtensionDocTest.java
+++ src/test/java/hello/tis/specificextensionblocking/documentation/ExtensionDocTest.java
@@ -3,9 +3,7 @@ package hello.tis.specificextensionblocking.documentation;
 import org.junit.jupiter.api.Test;
 
 /**
- * 확장자 리스트 조회(GET : /extensions)*
- * 확장자 추가(POST : /extensions)*
- * 확장자 삭제(DELETE : /extensions)*
+ * 확장자 리스트 조회, 추가, 삭제를 문서화합니다.*
  */
 class ExtensionDocTest {
 
