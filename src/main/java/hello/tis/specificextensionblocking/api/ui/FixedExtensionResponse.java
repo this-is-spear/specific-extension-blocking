95791cf4c6753d50f394a10ef6417b84faef4e10
this-is-spear <rjsckdd12@gmail.com>
2022-11-23 14:52:34 +0900

    Feat : Added Find API

---
+++ src/main/java/hello/tis/specificextensionblocking/api/ui/FixedExtensionResponse.java
@@ -0,0 +1,14 @@
+package hello.tis.specificextensionblocking.api.ui;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 고정 확장자 응답 값입니다. 이름과 포함 여부 정보를 가집니다.
+ */
+@Getter
+@RequiredArgsConstructor
+public class FixedExtensionResponse {
+  private final String name;
+  private final boolean checked;
+}
--- src/test/java/hello/tis/specificextensionblocking/documentation/ExtensionDocTest.java
+++ src/test/java/hello/tis/specificextensionblocking/documentation/ExtensionDocTest.java
@@ -1,28 +1,80 @@
 package hello.tis.specificextensionblocking.documentation;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import hello.tis.specificextensionblocking.api.service.ExtensionService;
+import hello.tis.specificextensionblocking.api.ui.CustomExtensionResponse;
 import hello.tis.specificextensionblocking.api.ui.ExtensionController;
+import hello.tis.specificextensionblocking.api.ui.ExtensionResponses;
+import hello.tis.specificextensionblocking.api.ui.FixedExtensionResponse;
+import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+import org.springframework.restdocs.operation.preprocess.Preprocessors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 /**
  * 확장자 리스트 조회, 추가, 삭제를 문서화합니다.*
  */
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(ExtensionController.class)
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
 class ExtensionDocTest {
 
-  @InjectMocks
-  ExtensionController extensionController;
+  @Autowired
+  MockMvc mockMvc;
 
   @MockBean
   ExtensionService extensionService;
 
   @Test
-  void findExtension() {
+  void findExtension() throws Exception {
+    // given
+    List<CustomExtensionResponse> customExtensionResponses = List.of(
+        new CustomExtensionResponse("md"),
+        new CustomExtensionResponse("git"));
+    List<FixedExtensionResponse> fixedExtensionResponses = List.of(
+        new FixedExtensionResponse("sh", true),
+        new FixedExtensionResponse("bash", true),
+        new FixedExtensionResponse("exe", false)
+    );
 
+    ExtensionResponses value = new ExtensionResponses(
+        fixedExtensionResponses,
+        customExtensionResponses
+    );
+
+    // when
+    when(extensionService.findAll()).thenReturn(value);
+
+    // then
+    MockHttpServletRequestBuilder builder = RestDocumentationRequestBuilders
+        .get("/extensions")
+        .accept(MediaType.APPLICATION_JSON);
+
+    mockMvc.perform(builder).andDo(MockMvcResultHandlers.print())
+        .andExpect(status().isOk())
+        .andDo(
+            document(
+                "create",
+                getDocumentRequest(),
+                getDocumentResponse(),
+                pathParameters()
+            )
+        );
   }
 
   @Test
@@ -33,5 +85,19 @@ class ExtensionDocTest {
   @Test
   void deleteExtension() {
 
+  }
+
+  private OperationResponsePreprocessor getDocumentResponse() {
+    return Preprocessors.preprocessResponse(Preprocessors.prettyPrint());
+  }
+
+  private OperationRequestPreprocessor getDocumentRequest() {
+    return Preprocessors.preprocessRequest(
+        Preprocessors.modifyUris()
+            .scheme("http")
+            .host("localhost")
+            .removePort(),
+        Preprocessors.prettyPrint()
+    );
   }
 }
