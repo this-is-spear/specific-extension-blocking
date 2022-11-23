219c7539d71afa519a2aad239003ef3c061ae8c0
this-is-spear <rjsckdd12@gmail.com>
2022-11-23 09:40:02 +0900

    Refact : Checked lint

--- src/main/java/hello/tis/specificextensionblocking/HealthController.java
+++ src/main/java/hello/tis/specificextensionblocking/HealthController.java
@@ -5,9 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 빌드한 파일이 잘 동작하는지 확인하기 위한 컨트롤러입니다.
- * 해당 클래스에는 더 이상 API를 추가하지 않습니다.
- * 헬스 체크 URI 는 `/health` 입니다.
+ * 빌드한 파일이 잘 동작하는지 확인하기 위한 컨트롤러입니다. 해당 클래스에는 더 이상 API를 추가하지 않습니다. 헬스 체크 URI 는 `/health` 입니다.
  */
 @RestController
 public class HealthController {
